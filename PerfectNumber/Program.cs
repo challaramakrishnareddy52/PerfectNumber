f07eaf1353c5e5c1fece8999c30b2a3a24f4ff93
challaramakrishnareddy52 <challaramakrishnareddy52@gmail.com>
2022-11-17 01:23:34 +0530

    Perfect Number

---
+++ PerfectNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace PerfectNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, i, sum = 0;
+            Console.WriteLine("Enter a number:");
+            i = 1;
+            number=int.Parse(Console.ReadLine());
+            while(i<number)
+            {
+                if(number % i == 0)
+                    sum+= i;
+                i++;
+            }
+            if (sum == number)
+                Console.WriteLine("it is a perfect number", number);
+            else
+                Console.WriteLine("it is not a perfect number", number);
+        }
+    }
+}
